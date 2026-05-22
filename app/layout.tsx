efbda25f9996f6b8745d292275db34c83f7fd652
sourav <saifactplanet@gmail.com>
2026-05-22 16:39:17 +0530

    light-theme-changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
         <SessionProviderWrapper>
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
