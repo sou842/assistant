fcc6987b1c645005d3bd17a062d3a2dfeabd1492
animesh-0041 <ghoroianimesh1508@gmail.com>
2026-05-30 00:16:43 +0530

    feat: Setup oAuth

--- auth.config.ts
+++ auth.config.ts
@@ -6,20 +6,6 @@ export const authConfig = {
   session: {
     strategy: "jwt",
   },
-  callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        token.id = user.id;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      if (token.id && session.user) {
-        session.user.id = token.id as string;
-      }
-      return session;
-    }
-  },
   pages: {
     signIn: "/login",
   },
